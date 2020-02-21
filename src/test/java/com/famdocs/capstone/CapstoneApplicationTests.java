62e93602f28cd0ec08f2ada256f69adaff651de1
Christina Guthrie <cguthrie00b@gmail.com>
2020-02-21 13:41:10 -0600

    updated tests

--- src/test/java/com/famdocs/capstone/CapstoneApplicationTests.java
+++ src/test/java/com/famdocs/capstone/CapstoneApplicationTests.java
@@ -1,17 +1,60 @@
 package com.famdocs.capstone;
 
-import org.junit.jupiter.api.Test;
+import com.famdocs.capstone.core.Quote;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class CapstoneApplicationTests {
 
+	QuoteDAO dao;
+	List<Quote> list;
+	Quote quote;
+	String resultQuote;
+	long id;
+
+	@Before
+	public void setUp(){
+		dao = Mockito.mock(QuoteDAO.class);
+		quote = Mockito.mock(Quote.class);
+		list = Mockito.mock(List.class);
+		id = 5;
+	}
+
+	@After()
+	public void tearDown(){
+		dao = null;
+		quote = null;
+		id = 0;
+		resultQuote = null;
+	}
+//	@Test
+//	void contextLoads() {
+//	}
+
 	@Test
-	void contextLoads() {
+	void getQuotesList() {
+		List<Quote> resultList = (List<Quote>) Mockito.when(dao.getQuotes()).thenReturn(list);
+		assertEquals(resultList, list);
+
 	}
 
 	@Test
-	void getQuote() {
+	void getQuote(long id) {
+		Quote result = (Quote) Mockito.when(dao.getQuote(id)).thenReturn(quote);
+		assertEquals(result, quote);
+	}
+
+	@Test
+	void add(Quote quote) {
 
 	}
 }
